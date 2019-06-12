8335059b0503d5a572c77e09e9b9ce810687e04f
tanveer <tanveerctg2014@gmail.com>
2019-06-12 18:05:26 +0600

    2nd commit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,54 +1,13 @@
 import React, { Component } from 'react'
 import classes from './NavBar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-// function NavBar() {
-
-//   return (
-//     <nav className={classes.navContainer} >
-//         <div  className={classes.nav}>
-//             <div  className={classes.logo}>
-//                 <img src="https://www.trainingexpress.org.uk/wp-content/uploads/2019/05/logo-final-1.png" id="header_logo" alt="Training Express" style={{width:'90%'}}></img>
-//             </div>
-//             <div className={classes.navItem}>
-//               <span className={classes.item1}>
-//                   <span style={{marginRight:'5px'}}>Courses</span>
-//                   <FontAwesomeIcon
-//                     icon={['fas','chevron-down']}
-//                     className={classes.downIcon}
-//                     />
-//               </span>
-//               <span className={classes.course}>
-//                   Training Teams
-//               </span>
-//               <span className={classes.course}>
-//                   Help
-//               </span>
-//               <span className={classes.course}>
-//                   Contact
-//               </span>
-//             </div>
-        
-//             <div className={classes.search}>
-//                     <FontAwesomeIcon
-//                     icon={['fas','search']}
-//                     transform="grow-1" 
-//                     className={classes.searchIcon}
-//                     />
-//                 <input type="text" placeholder="What do you want to know ?" className={classes.input}/>
-//             </div>
-
-//         </div>
-//     </nav>
-//   )
-// }
-
-// export default NavBar;
+import traing_logo from '../../img/Training-Express-White.png';
 
 export default class NavBar extends Component {
     
     render() {
         return (
+            <React.Fragment>
             <nav className={classes.navContainer} >
             <div  className={classes.nav}>
                 <div  className={classes.logo}>
@@ -84,6 +43,7 @@ export default class NavBar extends Component {
     
             </div>
         </nav>
+        </React.Fragment>
         )
     }
 }
