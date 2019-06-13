a0d0f81af278f33373553628ac496dd86dc05d73
tanveer <tanveerctg2014@gmail.com>
2019-06-13 12:54:56 +0600

    home page almost done

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,43 +7,45 @@ export default class NavBar extends Component {
     
     render() {
         return (
-            <React.Fragment>
             <nav className={classes.navContainer} >
-            <div  className={classes.nav}>
-                <div  className={classes.logo}>
-                    <img src="https://www.trainingexpress.org.uk/wp-content/uploads/2019/05/logo-final-1.png" id="header_logo" alt="Training Express" style={{width:'95%'}}></img>
+                <div  className={classes.nav}>
+                    <div  className={classes.logo}>
+                        <img src="https://www.trainingexpress.org.uk/wp-content/uploads/2019/05/logo-final-1.png" id="header_logo" alt="Training Express" style={{width:'95%'}}></img>
+                    </div>
+                    <div className={classes.navItem}>
+                        <span className={classes.item1}>
+                            <span style={{marginRight:'5px'}}>Courses</span>
+                            <FontAwesomeIcon
+                                icon={['fas','chevron-down']}
+                                className={classes.downIcon}
+                                />
+                        </span>
+                        <span className={classes.course}>
+                            Training Teams
+                        </span>
+                        <span className={classes.course}>
+                            Help
+                        </span>
+                        <span className={classes.course}>
+                            Contact
+                        </span>
+                    </div>
+                
+                    <div className={classes.search}>
+                            <FontAwesomeIcon
+                            icon={['fas','search']}
+                            transform="grow-1" 
+                            className={classes.searchIcon}
+                            />
+                        <input type="text" placeholder="What do you want to know ?" className={classes.input}/>
+                    </div>
+                    
+                    <div className={classes.signinsignup}>
+                        <button className={classes.login}>Login</button>
+                        <button className={classes.signUp}>Sign Up</button>
+                    </div>
                 </div>
-                <div className={classes.navItem}>
-                  <span className={classes.item1}>
-                      <span style={{marginRight:'5px'}}>Courses</span>
-                      <FontAwesomeIcon
-                        icon={['fas','chevron-down']}
-                        className={classes.downIcon}
-                        />
-                  </span>
-                  <span className={classes.course}>
-                      Training Teams
-                  </span>
-                  <span className={classes.course}>
-                      Help
-                  </span>
-                  <span className={classes.course}>
-                      Contact
-                  </span>
-                </div>
-            
-                <div className={classes.search}>
-                        <FontAwesomeIcon
-                        icon={['fas','search']}
-                        transform="grow-1" 
-                        className={classes.searchIcon}
-                        />
-                    <input type="text" placeholder="What do you want to know ?" className={classes.input}/>
-                </div>
-    
-            </div>
         </nav>
-        </React.Fragment>
         )
     }
 }
